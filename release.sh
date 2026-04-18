65040d2771b75f6a53ac780dca68b241b73323d4
wings <102859442+winaviation@users.noreply.github.com>
2026-04-18 12:50:20 +0700

    remove release.sh

--- release.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-make clean
-make package ARCHS="arm64 arm64e" TARGET="iphone:clang:latest:14.0" FINALPACKAGE=1 THEOS_PACKAGE_SCHEME=rootless
-make clean
-make package ARCHS="arm64 arm64e" TARGET="iphone:clang:latest:14.0" FINALPACKAGE=1
-make clean
-# this only works if you got the roothide theos fork: https://github.com/roothide/Developer
-make package ARCHS="arm64 arm64e" TARGET="iphone:clang:latest:14.0" FINALPACKAGE=1 THEOS_PACKAGE_SCHEME=roothide
